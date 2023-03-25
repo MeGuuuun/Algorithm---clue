405579dbab59f5ccfa5c41b585b10cd2435d03db
Jimin Park <55971771+MeGuuuun@users.noreply.github.com>
2023-03-25 17:01:30 +0900

    Update 다음에 올 숫자.java

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -5,6 +5,7 @@ class Solution {
         //등차수열 확인
         if(common[1]-common[0] == common[2]-common[1])
             return answer = common[common.length-1] + (common[1]-common[0]);
+        //등비수열 
         else
             return answer = common[common.length-1] * (common[1]/common[0]);
     }
