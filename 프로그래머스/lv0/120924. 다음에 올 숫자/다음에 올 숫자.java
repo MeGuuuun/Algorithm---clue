7fa001f4be6290ae8ad7088fa771346a6cfbed17
Jimin Park <55971771+MeGuuuun@users.noreply.github.com>
2023-03-25 17:00:31 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        
+        //등차수열 확인
+        if(common[1]-common[0] == common[2]-common[1])
+            return answer = common[common.length-1] + (common[1]-common[0]);
+        else
+            return answer = common[common.length-1] * (common[1]/common[0]);
+    }
+}
