e31913b732d5e57753673fe8bd7e49ca7e529a95
박지민 <55971771+MeGuuuun@users.noreply.github.com>
2024-04-30 17:25:26 +0900

    [Bronze II] Title: 블랙잭, Time: 220 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,31 +1,48 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-       
-        int[] num = new int[n];
-        
-        for(int i=0; i<n; i++){
-            num[i] = sc.nextInt();
-        }
-        
-        int sum = 0;
-        int tmp = 0;
-        
-        for(int i=0; i<n-2; i++) {
-            for(int j=i+1; j<n-1; j++) {
-                for(int k=j+1; k<n; k++){
-                    sum = num[i] + num[j] + num[k];
-                    if(sum <=m && sum>tmp){
-                        tmp = sum;
-                    }
-                }
-            }
-        }
-        System.out.println(tmp);
-    }
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[] card = new int[n];
+
+		sc.nextLine();
+		for (int i = 0; i < n; i++) {
+			card[i] = sc.nextInt();
+		}
+
+		int tmp = 0;
+
+		for (int i = 0; i < n - 2; i++) {
+			int sum = 0;
+
+			if (card[i] >= m)
+				continue;
+			for (int j = i + 1; j < n - 1; j++) {
+				sum = card[i];
+				if (sum + card[j] >= m) {
+					continue;
+				}
+					
+				for (int k = j + 1; k < n; k++) {
+					sum=card[i]+card[j];
+					if (sum + card[k] > m) {
+						continue;
+					}
+						
+					sum += card[k];
+					if (tmp < sum) {
+						tmp = sum;
+					}
+				}
+			}
+		}
+		System.out.println(tmp);
+
+	}
+
 }
