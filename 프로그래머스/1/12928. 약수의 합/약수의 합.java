a609e40cc81c23d1f1f0b7e6cf5a8885c95d18cb
박지민 <55971771+MeGuuuun@users.noreply.github.com>
2024-08-08 10:50:06 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int num=1; num<=n ; num++){
+            if(n%num==0) answer += num;
+        }
+        
+        return answer;
+    }
+}
