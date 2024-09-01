f79d41c2a9202952b47e7398576ca2828c2bb748
박지민 <55971771+MeGuuuun@users.noreply.github.com>
2024-09-02 02:31:26 +0900

    Update 다리 놓기.java

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -5,7 +5,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         
         int t = sc.nextInt();
-        int[][] arr = new int[t][2];
         
         for(int i=0; i<t; i++){
             int n = sc.nextInt();
