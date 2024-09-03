afa50718e0e0fdbe793c3f8f03caba53e885ddf7
박지민 <55971771+MeGuuuun@users.noreply.github.com>
2024-09-03 14:04:05 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 25.20 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        
+        for(int i=0; i<n/2; i++){
+            answer += "수박";
+        }
+        
+        answer = n%2==0 ? answer + "" : answer +"수";
+        
+        return answer;
+    }
+}
